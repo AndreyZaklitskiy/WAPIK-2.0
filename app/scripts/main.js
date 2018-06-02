9f30958eef53916ceab40dec16be951f07792e0f
Andrey Zaklitskiy <glockman94@gmail.com>
2018-06-02 11:22:41 +0300

    error

---
+++ app/scripts/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $('.team-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          prevArrow: '<button type="button" class="slickPrev"></button>',
+          nextArrow: '<button type="button" class="slickNext"></button>'
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          prevArrow: '<button type="button" class="slickPrev"></button>',
+          nextArrow: '<button type="button" class="slickNext"></button>'
+        }
+      }
+    ]
+});
+});
